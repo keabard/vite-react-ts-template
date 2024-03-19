2b4f94945afdb78a2887128e0e5be02a8c968c2f
Antoine Le Taxin <antoine.letaxin@wescale.fr>
2024-03-19 12:00:13 +0100

    chore: add some eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -38,12 +38,50 @@ module.exports = {
     'simple-import-sort/imports': 'error',
     'simple-import-sort/exports': 'error',
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'react/jsx-key': 'error',
     'unused-imports/no-unused-imports': 'error',
-    '@typescript-eslint/consistent-type-imports': 'error',
     '@stylexjs/valid-styles': ['error'],
+    'padding-line-between-statements': [
+      'error',
+      {
+        blankLine: 'always',
+        next: 'return',
+        prev: '*',
+      },
+    ],
+    '@typescript-eslint/consistent-type-imports': 'error',
     '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
     '@typescript-eslint/no-meaningless-void-operator': 'off',
     '@typescript-eslint/no-confusing-void-expression': ['error', { ignoreVoidOperator: true }],
+    '@typescript-eslint/array-type': [
+      'error',
+      {
+        default: 'array',
+      },
+    ],
+    '@typescript-eslint/no-unnecessary-boolean-literal-compare': 'error',
+    '@typescript-eslint/no-unnecessary-condition': 'error',
+    '@typescript-eslint/no-unnecessary-type-arguments': 'error',
+    '@typescript-eslint/no-unsafe-declaration-merging': 'error',
+    '@typescript-eslint/prefer-for-of': 'error',
+    '@typescript-eslint/prefer-includes': 'error',
+    '@typescript-eslint/prefer-optional-chain': 'error',
+    '@typescript-eslint/prefer-reduce-type-parameter': 'error',
+    '@typescript-eslint/prefer-string-starts-ends-with': 'error',
+    '@typescript-eslint/prefer-ts-expect-error': 'error',
+    '@typescript-eslint/no-floating-promises': 'error',
+    '@typescript-eslint/no-misused-promises': [
+      'error',
+      {
+        checksVoidReturn: {
+          attributes: false,
+        },
+      },
+    ],
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/consistent-type-exports': 'error',
+    '@typescript-eslint/no-redundant-type-constituents': 'warn',
   },
   overrides: [
     {
